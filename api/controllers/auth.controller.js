7443f5662c3b728f6e5c6e88dc25979a52063e45
Myo Myat Min <myomyatmin315@gmail.com>
2023-12-19 21:28:45 +0700

    create a middleware function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,10 +10,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("User created successfully.");
   } catch (error) {
-    if (error.code === 11000) {
-      res.status(400).json("Username or email already exists.");
-    } else {
-      res.status(500).json(error.message);
-    }
+    next(error);
   }
 };
